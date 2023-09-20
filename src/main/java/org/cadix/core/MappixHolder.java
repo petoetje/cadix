10ad0776e6c3175cbc58729b6da2d6b5cd684373
christo <christo@localhost>
2023-09-20 19:44:58 +0000

    - introduce mappix

--- src/main/java/org/cadix/core/MappixHolder.java
+++ src/main/java/org/cadix/core/MappixHolder.java
@@ -8,6 +8,7 @@ import jakarta.faces.application.ResourceDependencies;
 import jakarta.faces.application.ResourceDependency;
 import jakarta.faces.component.FacesComponent;
 import jakarta.faces.component.NamingContainer;
+import jakarta.faces.component.UIComponent;
 import jakarta.faces.component.UIOutput;
 import jakarta.faces.context.FacesContext;
 import java.io.IOException;
@@ -21,6 +22,7 @@ import org.json.simple.JSONValue;
  */
 @ResourceDependencies({
     @ResourceDependency(library = "mappix", name = "mappix.js", target = "head"),
+    @ResourceDependency(library = "mappix", name = "observer.js", target = "head"),
     @ResourceDependency(library = "jakarta.faces", name = "faces.js", target = "head") // Required for faces.ajax.request.
 })
 @FacesComponent(createTag = true, tagName = "mappixHolder")
@@ -38,7 +40,7 @@ public class MappixHolder extends UIOutput implements NamingContainer {
     }
 
     enum ContextKeys {
-        ACTIVEHOLDER
+        ACTIVEHOLDER, NAME
     }
 
     public HolderType getHolderType() {
@@ -64,12 +66,16 @@ public class MappixHolder extends UIOutput implements NamingContainer {
         // callback at the end
         String activeId = (String) context.getAttributes().get(ContextKeys.ACTIVEHOLDER);
         if (activeId == null) {
-            //use Id, because I think "this" is sometimes re-used , for multiple renders of the same component
+            //use Id, because I think "this" Java pointer is sometimes re-used , for multiple renders of the same component
             context.getAttributes().put(ContextKeys.ACTIVEHOLDER, getId());
             //todo : start Recoil object
         }
-
         //if I have Name then store it it context.  A child element might need it to call an updater
+        String myName = getName();
+        if (myName != null && !myName.isBlank()) {
+            context.getAttributes().put(ContextKeys.NAME, myName);
+        }
+
         //super.encodeBegin(context);
         //create a element with specified id, to make Mojarra happy
         //if we don't do this then partial updates don't work
@@ -93,28 +99,42 @@ public class MappixHolder extends UIOutput implements NamingContainer {
         //super.encodeEnd(context); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
         context.getResponseWriter().endElement("span");
         String activeId = (String) context.getAttributes().get(ContextKeys.ACTIVEHOLDER);
+        //if "activeId" === my Id then I was the first to start rendering
+        //so I am the last to end rendering
+        //this means I have to define the Recoil atom (if it does not exist yet), or call the setter
         if (activeId != null && activeId.equals(getId())) {
             context.getAttributes().remove(ContextKeys.ACTIVEHOLDER);
             List<String> splitId = Arrays.asList(getClientId().split(":"));
             String idArr = JSONValue.toJSONString(splitId);
-            //if we have a name, then define an atom
+            //if we have a name, then define an atom, if it does not exit yet (atom could have survived complete re-render of entire component)
             //else call setter
             String myName = getName();
+
+            if (myName == null) {
+                //I am the last one to render, but I have no name ---> we are partially re-rendering
+                //I do need the name of the current atom. Search the JSF tree for a parent with a name
+                //Look for the nearest MappixHolder with a name
+                UIComponent p;
+                while ((p = this.getParent()) != null && myName == null) {
+                    if (p instanceof MappixHolder) {
+                        myName = ((MappixHolder) p).getName();
+                        break;
+                    }
+                }
+            }
             if (myName != null) {
-                /**
-                 * const fontSizeState = atom({key: 'fontSizeState',default: 14
-                 * });
-                 */
-                context.getResponseWriter().startElement("script", this);
-                context.getResponseWriter().write(String.format("const %s = Recoil.atom({key: %s,default: JSON.stringify(getHolder(%s), mappxReplacer)});", myName, JSONValue.toJSONString(myName), idArr));
+                //setter should have been defined (in a previous render)
+                context.getResponseWriter().startElement("script",  this);
+                context.getResponseWriter().write("if (typeof window.mappixVals === 'undefined' ) { window.mappixVals = {} } ");
+                context.getResponseWriter().write(String.format("mappixSet('%s',JSON.parse(JSON.stringify(getHolder(%s), mappxReplacer)));",myName,idArr));
+                context.getResponseWriter().write(String.format("wobs.publish('%s');", myName));
                 context.getResponseWriter().endElement("script");
             } else {
-
+                //raise error ?
             }
-            //temp placeholde
-            context.getResponseWriter().startElement("script", this);
-            context.getResponseWriter().write("alert('END')");
-            context.getResponseWriter().endElement("script");
+
+        } else {
+            // no work to do
         }
     }
 
--- src/main/webapp/resources/mappix/mappix.js
+++ src/main/webapp/resources/mappix/mappix.js
@@ -1,47 +1,54 @@
 //add JSON.stringify support for Holder
 function mappxReplacer(key, value) {
-  if(value instanceof Holder) {
-    switch(value.holderType) {
-	case HolderType.list:
-		return Array.from(value.data.values()); // or with spread: value: [...value]
-		break;
-	default:
-	 return Array.from(value.data).reduce((obj, [key, value]) => {
-	    obj[key] = value;
-	    return obj;
-		  }, {});
+    if (value instanceof Holder) {
+        switch (value.holderType) {
+            case HolderType.list:
+                return Array.from(value.data.values()); // or with spread: value: [...value]
+                break;
+            default:
+                return Array.from(value.data).reduce((obj, [key, value]) => {
+                    obj[key] = value;
+                    return obj;
+                }, {});
+
+        }
+    } else if (value instanceof Map) {
+        return Array.from(value).reduce((obj, [key, value]) => {
+            obj[key] = value;
+            return obj;
+        }, {});
+
+    } else {
+        return value;
     }
-  } else {
-    return value;
-  }
 }
 
 
 const HolderType = {
-  map: 'Map',
-  list: 'List'
+    map: 'Map',
+    list: 'List'
 };
 
 class Holder {
-	constructor(holderType) { 
-	    this.#data = new Map();
-	    this.#holderType=holderType;
-        }
-	#holderType = null ;
-	get holderType() {
-		return this.#holderType;
-	}
-	set holderType(ht) {
-		this.#holderType=ht;
-	}	
-	#data
-	get data() {
-		return this.#data;
-	}
-	set data(d) {
-		this.#data=d;
-	}
-	
+    constructor(holderType) {
+        this.#data = new Map();
+        this.#holderType = holderType;
+    }
+    #holderType = null;
+    get holderType() {
+        return this.#holderType;
+    }
+    set holderType(ht) {
+        this.#holderType = ht;
+    }
+    #data
+    get data() {
+        return this.#data;
+    }
+    set data(d) {
+        this.#data = d;
+    }
+
 }
 
 
@@ -51,38 +58,38 @@ var roots = new Holder();
 //return the added Holder
 //if a holder already exists at indexArray,
 //then the current holder is returned
-function addHolder(indexArray,holderType) {
-	let curr = roots;
-	let i = 0;
-	while (i < indexArray.length) {
-		let index = indexArray[i];
-		let m=null;
-                //never add a holder when holderTYpe not set
-		if (!curr.data.has(index) && holderType !== null) {
-		        m = new Holder();
-			if (i < indexArray. length-1){
-				//intermediate must be map
-				m.holderType = HolderType.map;
-			} else {
-				m.holderType = holderType;	
-			}
-			curr.data.set(index,m);
-		}
-		curr=curr.data.get(index);
-		i++;
-	}	
-       return curr;	
+function addHolder(indexArray, holderType) {
+    let curr = roots;
+    let i = 0;
+    while (i < indexArray.length) {
+        let index = indexArray[i];
+        let m = null;
+        //never add a holder when holderTYpe not set
+        if (!curr.data.has(index) && holderType !== null) {
+            m = new Holder();
+            if (i < indexArray.length - 1) {
+                //intermediate must be map
+                m.holderType = HolderType.map;
+            } else {
+                m.holderType = holderType;
+            }
+            curr.data.set(index, m);
+        }
+        curr = curr.data.get(index);
+        i++;
+    }
+    return curr;
 }
 
 function addValue(indexArray, value) {
-	let p = indexArray.slice(0,-1);
- 	let holder = addHolder(p);	
-	holder.data.set(indexArray.slice(-1)[0],value);
-	return holder;
+    let p = indexArray.slice(0, -1);
+    let holder = addHolder(p);
+    holder.data.set(indexArray.slice(-1)[0], value);
+    return holder;
 }
 
 function addValueById(id, value) {
-	return addValue(id.split(":"), value);
+    return addValue(id.split(":"), value);
 }
 
 function getHolder(indexArray) {
@@ -90,3 +97,15 @@ function getHolder(indexArray) {
 }
 
 //console.log(JSON.stringify(roots, mappxReplacer));
+
+if (typeof mappixValues === 'undefined') {
+    mappixValues = {};
+} 
+
+function mappixGet(key) {
+    return typeof mappixValues[key] === 'undefined' ? null : mappixValues[key];
+}
+
+function mappixSet(key,value) {
+    mappixValues[key] = value;
+}
